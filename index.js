368720e40cb864db267973589fc3093693416a9c
Iain McKenzie <iainmck29@hotmail.com>
2018-08-30 09:18:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ function logShout(string) {
 
 var lowercase = 'hello'
 
+var uppercase = 'HELLO'
+
 function sayHiToGrandma(string) {
   if lowercase.toLowerCase() === lowercase
   return "I can't hear you"
-} else {
-  
+  }
+  else if {
+  uppercase.toUpperCase() === uppercase
+  return "YES INDEED!"
 }
