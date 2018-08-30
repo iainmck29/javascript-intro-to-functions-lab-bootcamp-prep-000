c18fd2680bf27ef86122c43db3b07c637a13f2f1
Iain McKenzie <iainmck29@hotmail.com>
2018-08-30 09:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ var lowercase = 'hello'
 var uppercase = 'HELLO'
 
 function sayHiToGrandma(string) {
-  if lowercase.toLowerCase() === lowercase
+  if (lowercase.toLowerCase() === lowercase)
   return "I can't hear you"
   }
-  else if {
-  uppercase.toUpperCase() === uppercase
-  return "YES INDEED!"
+ if (
+  uppercase.toUpperCase() === uppercase)
+  return "YES INDEED!";
 }
