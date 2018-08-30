26f329b8e7a42bd03ae978fc408d885d9ff0b624
Iain McKenzie <iainmck29@hotmail.com>
2018-08-30 09:57:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,6 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
   return"YES INDEED!"
   
-  if (string.)
+  if (string === "I love you, Grandma.")
+  return "I love you, too."
 }
